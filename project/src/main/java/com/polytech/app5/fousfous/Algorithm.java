43b6246aaba04ea29d113d1c805275f1a4da3834
Hussein Ait Lahcen <hussein.aitlahcen@cgi.com>
2017-11-21 10:21:20 +0000

    Add: Stupid algorithm computing the first stage of the graph only

--- project/src/main/java/com/polytech/app5/fousfous/Algorithm.java
+++ project/src/main/java/com/polytech/app5/fousfous/Algorithm.java
@@ -2,6 +2,7 @@
 package com.polytech.app5.fousfous;
 
 import java.util.*;
+import java.util.function.*;
 import com.polytech.app5.fousfous.play.*;
 
 public interface Algorithm {
@@ -12,17 +13,37 @@ public interface Algorithm {
     public static final int MIN_DEPTH = 2;
     public static final int MAX_DEPTH = 9;
 
+    public static final Algorithm TEUBE = (final Heuristic heuristic, final Board board, final Player player) -> {
+        final Set<Move> moves = board.getPossibleMoves(player);
+        System.out.println(Arrays.toString(moves.toArray()));
+        int bestScore = Integer.MIN_VALUE;
+        Move bestMove = null;
+        for(final Move move : moves) {
+            final int score = board.playAndCancel(move, (__) -> heuristic.computeScore(board, player));
+            System.out.println("computing move " + move + ", score=" + score + ", max=" + bestScore);
+            if(score > bestScore || bestMove == null) {
+                bestScore = score;
+                bestMove = move;
+            }
+        }
+        return bestMove;
+    };
+
     public static final Algorithm NEGAMAX = (final Heuristic heuristic, final Board board, final Player player) -> {
         final Set<Move> moves = board.getPossibleMoves(player);
         final int maxDepth = Integer.min(MAX_DEPTH, Integer.max(MIN_DEPTH, MAX_DEPTH - (int)Math.ceil(DEPTH_PER_MOVE * moves.size())));
         int bestScore = Integer.MIN_VALUE;
         Move bestMove = null;
         for(final Move move : moves) {
-            final int score = board.playAndCancel(move, (__) -> negaMax(heuristic, board, player.opponent(), maxDepth, -1));
-            if(score > bestScore) {
+            final int score = board.playAndCancel(move, (__) -> -negaMax(heuristic, board, player.opponent(), maxDepth, -1));
+            System.out.println("computing move " + move + ", score=" + score + ", max=" + bestScore);
+            if(score > bestScore || bestMove == null) {
                 bestScore = score;
                 bestMove = move;
             }
+        }
+        if(bestScore == Integer.MAX_VALUE) {
+            System.out.println("WE WIN WITH THIS MOVE");
         }
         return bestMove;
     };
