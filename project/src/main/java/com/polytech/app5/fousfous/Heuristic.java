7c73b4517e894f062a962606ac48a86b9ee3f9c4
Hussein Ait Lahcen <hussein.aitlahcen@cgi.com>
2017-11-21 10:20:56 +0000

    Add: Improved basic heuristic

--- project/src/main/java/com/polytech/app5/fousfous/Heuristic.java
+++ project/src/main/java/com/polytech/app5/fousfous/Heuristic.java
@@ -2,6 +2,7 @@ package com.polytech.app5.fousfous;
 
 import com.polytech.app5.fousfous.play.*;
 
+import java.util.stream.*;
 import java.util.*;
 
 public interface Heuristic {
@@ -10,13 +11,28 @@ public interface Heuristic {
 
     public static final Heuristic BASIC_WIN_LOSE = (board, player) -> {
         if(board.winner() == player.opponent()) {
-            return Integer.MIN_VALUE + 1;
+            return Integer.MIN_VALUE;
         }
         else if(board.winner() == player) {
-            return Integer.MAX_VALUE - 1;
+            return Integer.MAX_VALUE;
         }
         final int numberOfPawn = board.getNumberOfPawns(player.opponent());
-        return -numberOfPawn;
+        final int alignmentFactor = board.getAlignmentFactor(player);
+        final Stream<Move> possibleEnemyMoves = board.getPossibleMoves(player.opponent()).stream();
+        final Stream<Move> possibleAllyMoves = board.getPossibleMoves(player).stream();
+        final int possibleEnemyAttacks = (int)possibleEnemyMoves.filter(board::isAttackMovement).count();
+        final int possibleAllyAttaks = (int)possibleAllyMoves.filter(board::isAttackMovement).count();
+        return scoreGrade(-numberOfPawn, alignmentFactor, possibleAllyAttaks, possibleEnemyAttacks);
     };
 
+
+    public static int scoreGrade(final int... scores) {
+        int score = 0;
+        int power = (int)Math.pow(10, scores.length);
+        for(int i = 0; i < scores.length; i++) {
+            score += scores[i] * power;
+            power = (int)Math.floor(power / 10);
+        }
+        return score;
+    }
 }
